5c8ae896747353f4dd0385b48f0c6d81a25220cd
wldnd2 <jiwoong000628@gmail.com>
2024-09-20 22:29:35 +0900

    Add: 기본 정보 js파일 추가

---
+++ src/constants/ItemInfo.js
@@ -0,0 +1,40 @@
+// 제품 사진 url
+// https://travelshow.co.kr/shop_view/?idx=103
+
+export const ItemInfo = [
+  {
+    'imgPath': 'https://cdn.imweb.me/thumbnail/20230921/11e76181a966b.jpg',
+    'title': 'Temp',
+    'maker': 'Temp Maker, Inc.',
+    'price': '1000',
+    'description': 'This is a temporary description.',
+  },
+  {
+    'imgPath': 'https://cdn.imweb.me/thumbnail/20230921/11e76181a966b.jpg',
+    'title': 'Temp',
+    'maker': 'Temp Maker, Inc.',
+    'price': '1000',
+    'description': 'This is a temporary description.',
+  },
+  {
+    'imgPath': 'https://cdn.imweb.me/thumbnail/20230921/11e76181a966b.jpg',
+    'title': 'Temp',
+    'maker': 'Temp Maker, Inc.',
+    'price': '1000',
+    'description': 'This is a temporary description.',
+  },
+  {
+    'imgPath': 'https://cdn.imweb.me/thumbnail/20230921/11e76181a966b.jpg',
+    'title': 'Temp',
+    'maker': 'Temp Maker, Inc.',
+    'price': '1000',
+    'description': 'This is a temporary description.',
+  },
+  {
+    'imgPath': 'https://cdn.imweb.me/thumbnail/20230921/11e76181a966b.jpg',
+    'title': 'Temp',
+    'maker': 'Temp Maker, Inc.',
+    'price': '1000',
+    'description': 'This is a temporary description.',
+  }
+];
---
+++ src/constants/SanginInfo.js
@@ -0,0 +1,39 @@
+export const SanginInfo = [
+  {
+    'name': 'Im Sangin',
+    'imgPath': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg',
+    'description': "— I'll be in your neighborhood doing errands this…",
+  },
+  {
+    'name': 'Im Sangin',
+    'imgPath': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg',
+    'description': "— I'll be in your neighborhood doing errands this…",
+  },
+  {
+    'name': 'Im Sangin',
+    'imgPath': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg',
+    'description': "— I'll be in your neighborhood doing errands this…",
+  },
+  {
+    'name': 'Im Sangin',
+    'imgPath': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg',
+    'description': "— I'll be in your neighborhood doing errands this…",
+  },
+  {
+    'name': 'Im Sangin',
+    'imgPath': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg',
+    'description': "— I'll be in your neighborhood doing errands this…",
+  },
+  {
+    'name': 'Im Sangin',
+    'imgPath': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg',
+    'description': "— I'll be in your neighborhood doing errands this…",
+  },
+  {
+    'name': 'Im Sangin',
+    'imgPath': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg',
+    'description': "— I'll be in your neighborhood doing errands this…",
+  },
+];
+
+export default SanginInfo;
