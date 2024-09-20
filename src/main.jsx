8b98186bd8a2cf031ba1c54a28cdf303013f5d77
wldnd2 <jiwoong000628@gmail.com>
2024-09-20 22:14:37 +0900

    Feat: 기본 설정 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GlobalStyles } from './styles/GlobalStyles'
 import App from './App.jsx'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>,
 )
