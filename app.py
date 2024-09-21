7f4309572e8d3bc8f882b1cea2e6a3a3feb23299
wldnd2 <jiwoong000628@gmail.com>
2024-09-22 02:27:14 +0900

    Feat: firebase 연결 및 DB 읽어오기 구현

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 from pprint import pprint
 from flask import Flask, render_template, request
 from sentence_transformers import SentenceTransformer, util
+import firebase_admin
+from firebase_admin import credentials, db
 
 app = Flask(__name__)
 
@@ -12,13 +14,29 @@ df = pd.read_csv('./Dataprocess/DataFrame2.csv')
 # SBERT 모델 로드
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
+def database_init():
+    try:
+        app = firebase_admin.get_app()
+    except ValueError as e:
+        cred = credentials.Certificate("daeguhackathon.json")
+        firebase_admin.initialize_app(cred, {
+            "databaseURL": "https://daeguhackathon-62d3b-default-rtdb.asia-southeast1.firebasedatabase.app/",
+            "storageBucket": f"{cred.project_id}.appspot.com"
+        })
+        print("Firebase Database Initialized")
+
+def read_all_data():
+    ref = db.reference('/')
+    data = ref.get()
+    return data
+
 @app.route('/')
 def home():
+    database_init()
     return render_template('index.html')
 
 @app.route('/match', methods=['POST'])
 def match():
-    # POST 요청으로부터 키워드 받기
     keywords = request.form.getlist('keywords')
     if len(keywords) != 5:
         return "5개의 키워드를 입력해주세요.", 400
@@ -43,5 +61,13 @@ def match():
     pprint(top_creators.to_dict(orient='records'))
     return render_template('match.html', creators=top_creators.to_dict(orient='records'))
 
+@app.route('/data', methods=['GET'])
+def data():
+    database_init()
+    data = read_all_data()
+    pprint(data)
+    return json.dumps(data, ensure_ascii=False, indent=4)
+
 if __name__ == '__main__':
+    database_init()
     app.run(debug=True)
