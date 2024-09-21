5af812e6d26be0a463dea7b7552e455fb09a36e9
wldnd2 <jiwoong000628@gmail.com>
2024-09-21 22:45:32 +0900

    Feat: 명사 추출 구현

--- DataProcess/Count.py
+++ DataProcess/Count.py
@@ -1,6 +1,7 @@
 import json
 import re
 from collections import Counter
+from konlpy.tag import Okt
 
 # JSON 파일 읽기
 with open('./DataProcess/creators_data.json', 'r', encoding='utf-8') as file:
@@ -23,3 +24,19 @@ with open('./DataProcess/word_frequencies.json', 'w', encoding='utf-8') as file:
     json.dump(sorted_word_frequencies, file, ensure_ascii=False, indent=4)
 
 print("단어 빈도수가 'word_frequencies.json' 파일에 저장되었습니다.")
+
+# 형태소 분석기 초기화
+okt = Okt()
+
+# 명사만 추출하여 새로운 딕셔너리 생성
+noun_frequencies = {}
+for word, count in sorted_word_frequencies.items():
+    nouns = okt.nouns(word)
+    if nouns:
+        noun_frequencies[word] = count
+
+# 결과를 JSON 형식으로 저장
+with open('./DataProcess/noun_frequencies.json', 'w', encoding='utf-8') as file:
+    json.dump(noun_frequencies, file, ensure_ascii=False, indent=4)
+
+print("명사 빈도수가 'noun_frequencies.json' 파일에 저장되었습니다.")
